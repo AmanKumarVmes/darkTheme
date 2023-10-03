c0ac6a634657c48fc91d69f1443f8c5343751786
Aman Kumar <aman.kumar2@vidyamantra.com>
2023-10-03 10:31:59 +0530

    dark Mode

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // darMode:'class',
+  content: ["./Working/**/index.html",],
+  darkMode:'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
